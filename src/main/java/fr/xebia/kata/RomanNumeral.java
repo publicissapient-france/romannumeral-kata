7612c7bb0102ef9b5cdb37388927c24f5be360fc
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2011-05-31 17:32:16 +0200

    Initial Commit

---
+++ src/main/java/fr/xebia/kata/RomanNumeral.java
@@ -0,0 +1,9 @@
+package fr.xebia.kata;
+
+public class RomanNumeral {
+
+    public String of(int value) {
+        return "too lazy, to think.";
+    }
+
+}
---
+++ src/test/java/fr/xebia/kata/RomanNumeralTest.java
@@ -0,0 +1,14 @@
+package fr.xebia.kata;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class RomanNumeralTest {
+
+    @Test
+    public void givenTrue_thenTellTheTruth() throws Exception {
+        assertThat(true).isTrue();
+    }
+
+}
